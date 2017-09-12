b716fac8014ad3fa3975046f9288957e02efcca9
Anthony Sneed <tony@tonysneed.com>
2017-09-12 16:47:45 -0500

    Customize ts-lib-helper. - Remove prettier. - Config main module. - Target ES2015. - Add Angular linting. - Update gitignore.

--- src/observable-entities-js.ts
+++ src/observable-entities-js.ts
@@ -1,4 +1,2 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-// import "core-js/fn/array.find"
-// ...
-export default class DummyClass {}
+export { HelloWorld } from './hello-world';
+export { ItalianHelloWorld } from './italian-hello-world';
