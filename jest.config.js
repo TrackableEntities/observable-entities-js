f259769a28edd674ac1abdb22cf47b4f197056aa
Anthony Sneed <anthony.sneed@hilti.com>
2019-06-22 18:26:43 -0500

    Decrease test coverage thresholds.

--- jest.config.js
+++ jest.config.js
@@ -11,10 +11,10 @@ const config = {
   testPathIgnorePatterns: ['dist', 'src/models'],
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
     },
   }
 }
