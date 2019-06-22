b387ab41dc4858d1682ff045fa79be61772483a2
Anthony Sneed <anthony.sneed@hilti.com>
2019-06-22 15:43:08 -0500

    Fix Jest config.

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+/**
+ * @type {Partial<jest.InitialOptions>}
+ */
+const config = {
+  preset: 'ts-jest',
+  rootDir: '.',
+  testMatch: [
+    '<rootDir>/src/**/?(*.)+(spec|test).ts?(x)',
+  ],
+  testPathIgnorePatterns: ['dist', 'src/models'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  }
+}
+
+module.exports = config
